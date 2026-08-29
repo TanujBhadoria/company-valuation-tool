7151651caab8ca02778be3776f8301b89e88a4fd
Tanuj <tanujsinghbhadoria@gmail.com>
2026-08-29 20:38:24 +0530

    Working Spring Boot + H2 setup with test endpoint

---
+++ src/main/java/com/tanuj/valuation_tool/ValuationToolApplication.java
@@ -0,0 +1,13 @@
+package com.tanuj.valuation_tool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValuationToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ValuationToolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tanuj/valuation_tool/ValuationToolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tanuj.valuation_tool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValuationToolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
